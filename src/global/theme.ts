d9ddeb6fde306f82b30a27a01718c4ac36b07ba3
Arthur Lopes <arthurlopescontact@gmail.com>
2025-01-08 22:07:19 -0300

    feat: change theme colors

--- src/global/theme.ts
+++ src/global/theme.ts
@@ -1,36 +1,36 @@
-import { DefaultTheme } from 'styled-components/native';
+import type { DefaultTheme } from 'styled-components/native';
 
 export const theme: DefaultTheme = {
   colors: {
     primary: {
-      main: '#FF8036',
-      gradient: '#FF8036',
+      main: '#C8102E',
+      gradient: '#FF5733',
     },
     background: {
-      main: '#1A2232',
+      main: '#0B0B0D',
     },
     surface: {
-      main: '#1F293D',
-      glass: '#1F293DB3',
-      fade: '',
-      deep: '#253554',
-      mainPressed: '#202B40',
+      main: '#1C1C1E',
+      glass: '#101013 ',
+      fade: '#3A3F4C',
+      deep: '#4B5768 ',
+      mainPressed: '#262A35',
     },
     text: {
-      main: '#FFFFFF',
-      muted: '#637394',
+      main: '#E5E5E5',
+      muted: '#A5A5A5',
       onPrimary: '#FFFFFF',
     },
     border: {
-      main: '#6D9EFF1A',
+      main: '#4B5768',
     },
     icon: {
-      main: '#637394',
+      main: '#A5A5A5',
     },
     semantic: {
       positive: '#14CC52',
       warning: '#E5A117',
-      disable: '#FF803699',
+      disable: '#C8102E99',
     },
   },
   fontSize: {
